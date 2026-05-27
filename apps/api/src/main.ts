d25c347f1a02291a15fab11ef9b353dfdbbf868f
yegor-ts <69nonesons@gmail.com>
2026-05-27 15:31:19 +0300

    fix: open cors for production

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-    credentials: true,
-  });
+  app.enableCors();
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
